4554fade80fb40276fe3727412eff2a541445119
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-26 07:25:17 +0530

    [Rohit] Add: Create new contact in address book

---
+++ Address_Book/AddressBook.cs
@@ -0,0 +1,12 @@
+﻿namespace Address_Book
+{
+    internal class AddressBook
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book. \n");
+
+            Contacts c = new Contacts("Rohit", "Pagar", "Nampur", "Nashik", "Maharashtra", 423204, 9130928097, "rohitpagar13@gmail.com");
+        }
+    }
+}
---
+++ Address_Book/Contacts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{
+    internal class Contacts
+    {
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private long zip;
+        private long phone;
+        private string email;
+
+        public Contacts(string firstName, string lastName, string address, string city, string state, long zip, long phone, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phone = phone;
+            this.email = email;
+        }
+
+
+    }
+}
